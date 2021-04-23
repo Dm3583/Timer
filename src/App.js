7470b142b1cf499d52d6ec59371e975e31f32b66
Dm3583 <pankovdm@gmail.com>
2021-04-23 15:41:59 +0300

    proje install

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  state = {
+    timer: 0
+  }
+
+
+
+  return (
+    <div className="App">
+      <p>{this.state.timer}</p>
+    </div>
+  );
+}
+
+export default App;
