541abb97841ceb5b83028a4337ea5748ff440902
Dm3583 <pankovdm@gmail.com>
2021-04-25 11:20:15 -0700

    complite

--- src/App.js
+++ src/App.js
@@ -1,160 +1,92 @@
-import react, { Component } from 'react';
+import react, { useState, useEffect, useRef } from 'react';
+import { interval, fromEvent } from 'rxjs';
+import { map, buffer, filter, debounceTime } from 'rxjs/operators';
 import './App.css';
-import timer from './utility/Timer';
 
+const TIMER_STATUS = {
+  START: 'start',
+  STOP: 'stop',
+  WAIT: 'wait'
+};
 
-const INIT_STATE = {
-  hours: '00',
-  minutes: '00',
-  seconds: '00'
-}
+const App = () => {
 
-const Timer = new timer();
+  const [seconds, setSeconds] = useState(0);
+  const [status, setStatus] = useState(TIMER_STATUS.STOP);
 
-console.log(Timer);
+  const waitBtn = useRef(null);
 
-class App extends Component {
-  state = {
-    ...INIT_STATE,
-    isTimerOn: true
-  }
+  useEffect(()=>{
 
-
-
-
-
-
-
-  timerId = null;
-  onClickMilliseconds = 0;
-  currentSeconds = 0;
-
-  resetSeconds = () => {
-    this.currentSeconds = 0;
-  };
-
-  pad(value) {
-    return String(value).padStart(2, '0');
-  };
-
-  formatTime = (sec) => {
-    const base = 60;
-    if (sec >= (base * base * 24)) {
-      this.setState({ ...INIT_STATE });
-      this.resetSeconds();
-      return;
-    };
-    const seconds = this.pad(sec % base);
-    const minutes = this.pad(Math.floor((sec / base) % base));
-    const hours = this.pad(Math.floor((sec / (base * base)) % (base * base)));
-
-    return {
-      seconds,
-      minutes,
-      hours
-    };
-  };
-
-  tick = (toRender) => {
-    const cdateObj = new Date();
-    let t = (cdateObj.getTime() - this.onClickMilliseconds) - (this.currentSeconds * 1000);
-    if (t > 999) {
-      this.currentSeconds++;
-    };
-    toRender({
-      ...this.formatTime(this.currentSeconds)
-    });
-  };
-
-  timerStop = (toRender, objToReset) => {
-    clearInterval(this.timerId);
-    this.resetSeconds();
-    toRender({ ...objToReset });
-    // this.setState({ ...INIT_STATE });
-  };
-
-
-  timerStart = (currSec = 0, funcToRender) => {
-    this.onClickMilliseconds = new Date().getTime() - currSec * 1000;
-    this.timerId = setInterval(this.tick, 1000, funcToRender);
-  };
+    const timer = interval(1000);
+    const subscription = timer.subscribe(()=>
+      {
+          if(status===TIMER_STATUS.START){
+            setSeconds(sec=>sec+1000);
+        };
+      }
+    );
+      return ()=> subscription.unsubscribe();
+  },
+  [status]);
 
   //запуск / остановка отсчета времени,
   //останавливает и обнуляет значение таймера.
-
-  handleStartStop = () => {
-    this.setState({ isTimerOn: !this.state.isTimerOn });
-    const setStateForRender = this.setState.bind(this);
-    if (this.state.isTimerOn && this.currentSeconds === 0) {
-      this.timerStart(0, setStateForRender);
-    } else if (this.state.isTimerOn && this.currentSeconds > 0) {
-      this.timerStart(this.currentSeconds, setStateForRender);
-    }
-    else {
-      this.timerStop(setStateForRender, INIT_STATE);
+    const handleStartStop = () => {
+      if(status === TIMER_STATUS.STOP||status === TIMER_STATUS.WAIT){
+      setStatus(TIMER_STATUS.START);
+      }else{
+      setStatus(TIMER_STATUS.STOP);
+      setSeconds(0);
+      }
     };
-  };
-
 
   //работает на двойной клик
   //(время между нажатиями не более 300 мс!)
   //таймер должен прекратить отсчет времени;
   //если после него нажать старт, то возобновляется отсчет.
+  const handleWait = () => {
 
-  timerWait = () => {
-    if (!this.timerId) {
-      return;
-    }
-    this.setState({ isTimerOn: !this.state.isTimerOn });
-    clearInterval(this.timerId);
+    const click = fromEvent(waitBtn.current, 'click');
+    const doubleClick = click
+    .pipe(
+      buffer(click.pipe(debounceTime(300))),
+      map(clicks => clicks.length),
+      filter(clicksLength => clicksLength === 2)
+    );
+    const subscription = doubleClick.subscribe(_ => {
+      setStatus(TIMER_STATUS.WAIT);    
+    });
+    return () => subscription.unsubscribe();
   };
-
 
   // сброс секундомера  на 0.
   //Обнуляет секундомер и снова начинает отсчет.
-  timerReset = () => {
-    // console.log(this.formatTime(this.currentSeconds));
-    const setStateForRender = this.setState.bind(this);
-    this.timerStop(setStateForRender, INIT_STATE);
-    this.timerStart(0, setStateForRender);
-  };
-
-
-
-
-
-
-
-
-
-
-
-
-
-  render() {
-    const { hours, minutes, seconds } = this.state;
-    const { handleStartStop, timerWait, timerReset } = this;
-    return (
-      <div className="App" >
-        <p>
-          <span>{hours} : </span>
-          <span>{minutes} : </span>
-          <span>{seconds}</span>
-        </p>
-        <div className="Controls">
-          <button type="button" onClick={handleStartStop}>
-            Start / Stop
-          </button>
-          <button type="button" onClick={timerWait}>
-            Wait
-          </button>
-          <button type="button" onClick={timerReset}>
-            Reset
-          </button>
-        </div>
+    const handleReset = () => {
+      if(status === TIMER_STATUS.WAIT&&seconds>0){
+        setStatus(TIMER_STATUS.START);
+      }
+      setSeconds(0);
+    };
+ 
+  return (
+    <div className="App" >
+      <p>
+          {new Date(seconds).toISOString().slice(11,19)}
+      </p>
+      <div className="Controls">
+        <button type="button" onClick={handleStartStop}>
+          {status !== TIMER_STATUS.START ? "Start" : "Stop"}
+        </button>
+        <button ref={waitBtn} type="button" onClick={handleWait}>
+          Wait
+        </button>
+        <button type="button" onClick={handleReset}>
+          Reset
+        </button>
       </div>
-    );
-  };
+    </div>
+  );
 };
 
 export default App;
